f9b50dee14330756e86a5a9786ce18d8c9b74c79
NP2420 <nat06pham@gmail.com>
2024-10-26 16:37:14 -0400

    Header menu and shit

--- script.js
+++ script.js
@@ -10,6 +10,49 @@ function main() {
     card.addTo(whiteboard);
 
     console.log(card);
+
+    //Header
+    const menuItems = Array.from(document.getElementsByClassName("menu"));
+    const submenuGroups = Array.from(document.getElementsByClassName("submenu"));
+    let menuActive = false;
+
+    resetMenus(menuItems, submenuGroups);
+    for (let i = 0; i < menuItems.length; i++) {
+        menuItems[i].addEventListener("click", function(ev) {
+            ev.stopPropagation();
+            resetMenus(menuItems, submenuGroups);
+            menuItems[i].style.borderRadius = "1vh 1vh 0vh 0vh";
+            menuItems[i].style.backgroundColor = "#cfdcef";
+            submenuGroups[i].style.display = "block";
+            menuItems[i].classList.remove("push");
+            menuActive = true;
+        })
+        menuItems[i].addEventListener("mouseenter", function() {
+            if (menuActive) {
+                ev.stopPropagation();
+                resetMenus(menuItems, submenuGroups);
+                menuItems[i].style.borderRadius = "1vh 1vh 0vh 0vh";
+                menuItems[i].style.backgroundColor = "#cfdcef";
+                submenuGroups[i].style.display = "block";
+                menuItems[i].classList.remove("push");
+            }
+        })
+    }
+
+    document.addEventListener("click", function(){
+        resetMenus(menuItems, submenuGroups);
+    })
+}
+
+function resetMenus(menuItems, submenuGroups) {
+    menuItems.forEach(ele => {
+        ele.style.borderRadius = "";
+        ele.style.backgroundColor = "";
+        ele.classList.add("push");
+    })
+    submenuGroups.forEach(ele => {
+        ele.style.display = "none";
+    })
 }
 
 window.onload = main;
