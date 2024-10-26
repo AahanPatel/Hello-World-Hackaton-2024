23dc5b75c7fb5f78537b3d0df67405c6114680ba
Daniel Quednow <danquednow@gmail.com>
2024-10-26 12:09:54 -0400

    Move addition to whiteboard
    Needed for text length stuff

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function main() {
     const whiteboard = document.getElementById("whiteboard");
 
     const card = new IdeaCard();
+    card.addTo(whiteboard);
 
     card.width = card.height = 100;
     card.x = card.y = 50;
     card.title = "Hi";
 
-    card.addTo(whiteboard);
 
     console.log(card);
 }
