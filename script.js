04af78e0576669aa6ec8ba2a12cf778df002c6e9
Daniel Quednow <danquednow@gmail.com>
2024-10-26 16:25:21 -0400

    Add an arrow and a second card

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function main() {
     const whiteboard = document.getElementById("whiteboard");
 
+    const cards = [];
+
     const card = new IdeaCard();
     card.addTo(whiteboard);
 
@@ -8,8 +10,7 @@ function main() {
     card.x = card.y = 50;
     card.title = "Hi";
 
-
-    console.log(card);
+    cards.push(card);
 
     const addButton = document.getElementsByClassName("add-circle-container")[0];
 
@@ -21,7 +22,16 @@ function main() {
         card.height = 40;
         card.x = card.y = 10;
         card.title = "New Idea!";
+
+        cards.push(card);
     });
+
+    addButton.click();
+
+    const arrow = new SolidArrow();
+    arrow.setBetweenCards(cards[0], cards[1]);
+
+    arrow.addTo(whiteboard);
 }
 
 window.onload = main;
