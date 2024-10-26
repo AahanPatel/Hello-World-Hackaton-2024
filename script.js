975062e60264419a22dea9921f331f8551e3ff6c
Ryan <ryanbjo187@gmail.com>
2024-10-26 12:00:55 -0400

    Added arrows and minor style changes

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ function main() {
     const whiteboard = document.getElementById("whiteboard");
 
     const card = new IdeaCard();
+    const solidArrow = new SolidArrow();
+    const dottedArrow = new DottedArrow();
+
+    solidArrow.setStartPoint(100, 100); 
+    solidArrow.setEndPoint(300, 200);
+    dottedArrow.setStartPoint(300, 200); 
+    dottedArrow.setEndPoint(100, 300);
+    solidArrow.addTo(whiteboard);
+    dottedArrow.addTo(whiteboard);
 
     card.width = card.height = 100;
     card.x = card.y = 50;
